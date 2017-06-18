b6ee917bac8a7f193ddc3c9ef2d3a06d46e55e35
Rin <cameron.j.l.nelson@gmail.com>
2017-06-18 17:12:39 -0500

    update homepage

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = models.BlogPost.objects.all
+
+    return render(request, 'blog/post_list.html', {'posts':posts})
