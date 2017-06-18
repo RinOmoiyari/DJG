4c84e70609c8454e19e3e32b7de1f63ca718c41c
Rin <cameron.j.l.nelson@gmail.com>
2017-06-18 18:34:22 -0500

    modified homepage to display posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
 from . import models
 
 # Create your views here.
 def post_list(request):
-    posts = models.BlogPost.objects.all
+    posts = models.BlogPost.objects.order_by('-published_date').filter(published_date__lte=timezone.now())
 
     return render(request, 'blog/post_list.html', {'posts':posts})
