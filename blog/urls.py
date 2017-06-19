c5fd6c29b6d8f6a3a605894d644208f88a599e3e
Rin <cameron.j.l.nelson@gmail.com>
2017-06-19 13:29:19 -0500

    Added post detail and publish function for unpublished posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_details, name='post_detail'),
+    url(r'^post/(?P<pk>\d+)/publish$', views.publish_post, name='publish'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from . import models
 
@@ -7,3 +7,13 @@ def post_list(request):
     posts = models.BlogPost.objects.order_by('-published_date').filter(published_date__lte=timezone.now())
 
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_details(request, pk):
+    #post = models.BlogPost.objects.get(pk=pk)
+    post = get_object_or_404(models.BlogPost, pk=pk)
+    return render(request, 'blog/post_details.html', {'post':post})
+
+def publish_post(request, pk):
+    post = get_object_or_404(models.BlogPost, pk=pk)
+    models.BlogPost.publish(post)
+    return redirect('post_detail', pk=pk)
